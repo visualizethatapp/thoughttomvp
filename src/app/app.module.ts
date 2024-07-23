6f552d3dd149e57836fcb0031ae78937262288ab
Mamello <mmoshoeshoe@webelieve.co.za>
2024-07-23 12:57:29 +0200

    set up pages and widgets module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WidgetsModule } from './widgets/widgets.module';
+import { PagesModule } from './pages/pages.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    WidgetsModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/widgets/widgets.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+@NgModule({
+  declarations: [
+    BannerComponent,
+    NavigationComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BannerComponent,
+    NavigationComponent
+  ]
+})
+export class WidgetsModule { }
